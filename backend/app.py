90394adb40ee34038fa0ab31794bf1cf4458f729
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-03-01 08:02:53 +0530

    refactor: app.py and requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -64,4 +64,4 @@ def create_app(env: str = "development") -> Flask:
 
 if __name__ == "__main__":
     app = create_app("development")
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
